cfdf6ead15623ed759567c8d1fafd519d0fff5a0
John Robertson <johnmagnusrobertson@gmail.com>
2023-03-08 21:03:02 +0000

    DEV-12, _app: add adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
+import Script from "next/script";
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-6809452858412935"
+        async
+        strategy="afterInteractive"
+        onError={(e) => {
+          // eslint-disable-next-line no-console
+          console.error("Script failed to load", e);
+        }}
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
