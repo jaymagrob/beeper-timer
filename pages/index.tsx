e23a28fca19218e07e41db54615d0f6145c8f1c0
John Robertson <johnmagnusrobertson@gmail.com>
2023-03-22 20:00:08 +0000

    DEV-14, settings: logic added

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,33 +5,44 @@ import Settings from '../components/settings';
 
 export default function Home() {
   const [time, setTime] = useState(0)
-  const [countdown, setCountdown] = useState(5)
+  const [countdown, setCountdown] = useState(time)
   const [nextBeep, setNextBeep] = useState(null);
   const router = useRouter();
+  const [startTime, setStartTimer] = useState(false)
+  const [interval, setInterval2] = useState(null)
 
   useEffect(() => {
     if (!router.isReady) return;
     const query = router.query;
     if (query.s != null) {
       setTime(query.s);
+      setCountdown(query.s);
     }
   }, [router.isReady]);
 
   useEffect(() => {
     // exit early when we reach 0
-    if (!countdown) return;
+    if(!startTime) return;
 
-    // save intervalId to clear the interval when the
-    // component re-renders
-    const intervalId = setInterval(() => {
-      setCountdown(countdown - 1);
-    }, 1000);
-
+    if (!countdown) {
+      setCountdown(time)
+      clearInterval(interval);
+      setInterval2(setInterval(() => {
+        setCountdown(countdown - 1);
+      }, 1000))
+    } else {
+      // save interval to clear the interval when the
+      // component re-renders
+      setInterval2(setInterval(() => {
+        setCountdown(countdown - 1);
+      }, 1000));
+    };
+      
     // clear interval on re-render to avoid memory leaks
-    return () => clearInterval(intervalId);
+    return () => clearInterval(interval);
     // add countdown as a dependency to re-rerun the effect
     // when we update it
-  }, [countdown]);
+  }, [countdown, startTime]);
 
 
   return (
@@ -39,8 +50,10 @@ export default function Home() {
       <Settings />
       <main>
       <h1>{time}</h1>
-      <Audio count={time} />
-      {countdown}
+      <button onClick={() => setStartTimer(true)}>Start Timer</button>
+      <Audio count={countdown} />
+      Time: {time}
+      Countdown: {countdown}
       </main>
     </>
   )
