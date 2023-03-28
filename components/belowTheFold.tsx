c5ae3d70c46e08a61d8a516a9bb12db3c73d1c69
John Robertson <johnmagnusrobertson@gmail.com>
2023-03-28 18:12:29 +0100

    DEV-15: belowTheFold: added

---
+++ components/belowTheFold.tsx
@@ -0,0 +1,16 @@
+export default function BelowTheFold() {
+  <>
+    <p>
+      Welcome to our website, where we bring you a simple yet effective tool to help you stay focused and on-task. Our 30 second beep app is designed to keep you alert and mindful of time, by emitting a beep sound every 30 seconds. Whether you&apos;re studying, working, or just need a gentle reminder to stay present, our app is here to support you.
+    </p>
+    <p>
+      With the increasing distractions in our daily lives, it can be challenging to maintain focus and productivity. Our app provides a constant reminder of time, helping you stay on track and accomplish your goals. By integrating this tool into your daily routine, you can increase your efficiency and get more done in less time.
+    </p>
+    <p>
+      Our app is user-friendly and easy to use. Simply press start and let the beeps guide you. You can customize the frequency and duration of the beeps to suit your preferences, and the app runs seamlessly in the background of your device. Plus, it&apos;s completely free to use!
+    </p>
+    <p>
+      Don&apos;t let distractions get the best of you. Try our 30 second beep app today and experience the benefits of increased focus and productivity.
+    </p>
+  </>
+}
