0c3b62de3e638a799fe2b94be25833f354549e87
John Robertson <johnmagnusrobertson@gmail.com>
2023-03-29 18:48:06 +0100

    DEV-17, config: fix ampliy issues.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  eslint: {
+    ignoreDuringBuilds: false,
+  },
 }
 
 module.exports = nextConfig
